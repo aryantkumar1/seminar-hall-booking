5f62eefa523d95b20949cb754fb71a69ab49fc33
Suprabath Reddy Immareddy <immareddysuprabathreddy@gmail.com>
2025-06-04 11:04:36 +0000

    Console Error
    In HTML, whitespace text nodes cannot be a child of <tr>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    This will cause a hydration error.
    
    See more info here: https://nextjs.org/docs/messages/react-hydration-error
    
    ...
        <ClientPageRoot Component={function AdminDashboardPage} searchParams={{}} params={{}}>
          <AdminDashboardPage params={Promise} searchParams={Promise}>
            <div>
              <PageTitle>
              <div className="grid lg:gr...">
                <div>
                <_c className="shadow-lg ...">
                  <div ref={null} className="rounded-lg...">
                    <_c2>
                    <_c8>
                      <div ref={null} className="p-6 pt-0">
                        <div className="max-h-[600...">
                          <_c>
                            <div className="relative w...">
                              <table ref={null} className="w-full cap...">
                                <_c2>
                                  <thead ref={null} className={"[&_tr]:b..."}>
                                    <_c8>
    >                                 <tr
    >                                   ref={null}
    >                                   className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-m..."
    >                                 >
                                        <_c10>
    >                                   {" "}
                                        ...
                                ...
    src/components/ui/table.tsx (59:3) @ _c8
    
      57 |   React.HTMLAttributes<HTMLTableRowElement>
      58 | >(({ className, children, ...props }, ref) => (
    > 59 |   <tr
         |   ^
      60 |     ref={ref}
      61 |     className={cn("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", className)}
      62 |     {...props}
    Call Stack
    16
    
    Show 13 ignore-listed frame(s)
    tr
    <anonymous> (0:0)
    _c8
    src/components/ui/table.tsx (59:3)
    AdminDashboardPage
    src/app/admin/dashboard/page.tsx (142:21)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -8,11 +8,7 @@ const Table = React.forwardRef<
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, children, ...props }, ref) => (
   <div className="relative w-full overflow-auto">
-    <table
-      ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
-      {...props}
-    >{children}</table>
+    <table ref={ref} className={cn("w-full caption-bottom text-sm", className)} {...props}>{children}</table>
   </div>
 ))
 Table.displayName = "Table"
@@ -29,11 +25,7 @@ const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, children, ...props }, ref) => (
-  <tbody
-    ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
-    {...props}
-  >{children}</tbody>
+  <tbody ref={ref} className={cn("[&_tr:last-child]:border-0", className)} {...props}>{children}</tbody>
 ))
 TableBody.displayName = "TableBody"
 
@@ -41,14 +33,7 @@ const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, children, ...props }, ref) => (
-  <tfoot
-    ref={ref}
-    className={cn(
-      "border-t bg-muted/50 font-medium [&>tr]:last:border-b-0",
-      className
-    )}
-    {...props}
-  >{children}</tfoot>
+  <tfoot ref={ref} className={cn("border-t bg-muted/50 font-medium [&>tr]:last:border-b-0", className)} {...props}>{children}</tfoot>
 ))
 TableFooter.displayName = "TableFooter"
 
@@ -56,11 +41,7 @@ const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
 >(({ className, children, ...props }, ref) => (
-  <tr
-    ref={ref}
-    className={cn("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", className)}
-    {...props}
-  >{children}</tr>
+  <tr ref={ref} className={cn("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", className)} {...props}>{children}</tr>
 ))
 TableRow.displayName = "TableRow"
 
@@ -68,14 +49,7 @@ const TableHead = React.forwardRef<
   HTMLTableCellElement,
   React.ThHTMLAttributes<HTMLTableCellElement>
 >(({ className, children, ...props }, ref) => (
-  <th
-    ref={ref}
-    className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
-      className
-    )}
-    {...props}
-  >{children}</th>
+  <th ref={ref} className={cn("h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0", className)} {...props}>{children}</th>
 ))
 TableHead.displayName = "TableHead"
 
@@ -83,11 +57,7 @@ const TableCell = React.forwardRef<
   HTMLTableCellElement,
   React.TdHTMLAttributes<HTMLTableCellElement>
 >(({ className, children, ...props }, ref) => (
-  <td
-    ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
-    {...props}
-  >{children}</td>
+  <td ref={ref} className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)} {...props}>{children}</td>
 ))
 TableCell.displayName = "TableCell"
 
@@ -95,11 +65,7 @@ const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
   React.HTMLAttributes<HTMLTableCaptionElement>
 >(({ className, children, ...props }, ref) => (
-  <caption
-    ref={ref}
-    className={cn("mt-4 text-sm text-muted-foreground", className)}
-    {...props}
-  >{children}</caption>
+  <caption ref={ref} className={cn("mt-4 text-sm text-muted-foreground", className)} {...props}>{children}</caption>
 ))
 TableCaption.displayName = "TableCaption"
 
